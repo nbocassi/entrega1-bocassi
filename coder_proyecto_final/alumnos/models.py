dd6f418e499f7edefaeb8dfd86cbbc7c467f92bc
Nicolas Bocassi <nicolasbocassi@gmail.com>
2022-09-04 18:05:30 -0300

    Creacion base de datos

--- coder_proyecto_final/alumnos/models.py
+++ coder_proyecto_final/alumnos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Alumnos(models.Model):
+    nombre = models.CharField(max_length=128)
+    apellido = models.CharField(max_length=128)
+
+
+class Ubicacion(models.Model):
+    provincia = models.CharField(max_length=128)
+    barrio = models.CharField(max_length=128)
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=128)
+    comision = models.IntegerField()
